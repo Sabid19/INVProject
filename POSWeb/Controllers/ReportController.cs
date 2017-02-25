ee96c732781630dade36ec5f5a003884a47727c2
Anuyel <Anuyel@DESKTOP-4AL5CRS>
2017-02-25 22:29:04 +0600

    stock  report

---
+++ POSWeb/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess.Repository;
+using DataAccess;
+using POSWeb.ViewModel.Stock;
+
+namespace POSWeb.Controllers
+{
+    public class ReportController : Controller
+    {
+        Repository<Stock> stkrepo = new Repository<Stock>();
+        // GET: Report
+        public ActionResult Index()
+        {
+          var stok=  stkrepo.GetAll();
+            return View(stok);
+        }
+
+        [HttpPost]
+        public ActionResult Index(StockSearchVM vm)
+        {
+            var filter = new StockFilter();
+            var model = filter.FilterStocks(vm);
+            return View(model);
+        }
+    }
+}
